9f3d8c0775d2a38e677482aa6548799be8170e5b
root <root@vagrant.vm>
2014-10-24 03:32:08 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <omp.h>
+#include <stdio.h>
+ 
+int main() {
+    #pragma omp parallel
+    printf("Hello, world! This is thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+}
