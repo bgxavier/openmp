f4b012b6252aaa5d99bc5b05d019131552e65aa4
Bruno Xavier <bgxavier@gmail.com>
2014-10-27 14:08:39 +0000

    odd

--- hello.c
+++ hello.c
@@ -1,7 +1,47 @@
 #include <omp.h>
 #include <stdio.h>
+#include <stdlib.h> 
+
+#define RANDOM_INTERVAL 100
+#define PRINT 1
+
+void OddEvenSort(int *A, int N);
+
+int main(int argc,char **argv) {
  
-int main() {
-    #pragma omp parallel
-    printf("Hello, world! This is thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+   int vector_size = atoi(argv[1]);
+   int *vector = (int *) malloc(sizeof(int)*vector_size);
+   int i;
+   srand(time(0));
+
+   for(i=0; i<vector_size; i++){
+        vector[i] = rand() % RANDOM_INTERVAL;
+   }
+
+   OddEvenSort( vector, vector_size);
+
+   if(PRINT){
+   	for(i=0;i<vector_size;i++)
+		printf("%d ", vector[i]);
+   }
+}
+
+void OddEvenSort( int *A, int N) {
+	int exch = 1;
+	int start = 0; 
+	int i; 
+	int temp;
+	while (exch || start) { 
+		exch = 0;
+		#pragma omp parallel for private(temp) 
+		for (i = start; i < N-1; i += 2) {
+			if (A[i]>A[i+1]){
+				temp = A[i]; 
+				A[i] = A[i+1]; 
+				A[i+1] = temp; 
+				exch = 1;
+			}
+		 }
+	if (start == 0) start = 1; else start = 0;
+	}
 }
