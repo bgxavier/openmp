f6ee1197f0dd0d3be7fd6d58460cca1a4e63ffba
Bruno Xavier <bgxavier@gmail.com>
2014-12-04 00:59:43 -0200

    1936

--- get_ncdc.sh
+++ get_ncdc.sh
@@ -3,7 +3,7 @@ DATASET_FOLDER="/Users/bgxavier/projects/OpenMP_MPI/dataset"
 
 #for i in {1954..1955}
 #do
-    wget -r -c -N -np -nH --cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$1/
+    wget -r -N -np -nH --cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$1/
     mkdir -p $DATASET_FOLDER/files/$1
     cp $1/*.gz $DATASET_FOLDER/files/$1/
     rm -rf $DATASET_FOLDER/$1
