051fc0def5b978792752266e585b4bdf1678c7e6
Bruno Xavier <bgxavier@gmail.com>
2014-12-04 00:28:37 -0200

    nova versao

--- get_ncdc.sh
+++ get_ncdc.sh
@@ -1,13 +1,11 @@
 #/usr/bin/bash
 DATASET_FOLDER="/Users/bgxavier/projects/OpenMP_MPI/dataset"
 
-for i in {1950..2013}
-do
-    cd $DATASET_FOLDER
-    mkdir -p files/$i
-    wget -r -np -nH --cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$i/
-    cd $i/
-    cp *.gz $DATASET_FOLDER/files/$i/
-    rm -rf $DATASET_FOLDER/$i
-done
+#for i in {1954..1955}
+#do
+    wget -r -c -N -np -nH --cut-dirs=3 -R index.html ftp://ftp.ncdc.noaa.gov/pub/data/noaa/$1/
+    mkdir -p $DATASET_FOLDER/files/$1
+    cp $1/*.gz $DATASET_FOLDER/files/$1/
+    rm -rf $DATASET_FOLDER/$1
+#done
 
--- temperature.c
+++ temperature.c
@@ -4,13 +4,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define YEAR_MIN 1920
-#define YEAR_MAX 1930
+#define YEAR_MIN 1910
+#define YEAR_MAX 1911
 #define DATASET_DIR "dataset/files/all/"
 #define KILLTAG 9999
+#define DONE_TAG 2
 
 void master();
 void slave();
+
+char* chop(char *string)
+{
+    int i, len;
+    len = strlen(string);
+    char *newstring;
+
+    newstring = (char *)malloc(len-1);
+
+    for(i = 0; i < strlen(string)-1; i++){
+        newstring[i] = string[i];
+    }
+
+    return newstring;
+}
 
 int main(int argc, char  *argv[])
 {
@@ -23,8 +39,9 @@ int main(int argc, char  *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    if(rank == 0)
+    if(rank == 0){
         master();
+    }
     else
         slave();
 
@@ -38,7 +55,10 @@ void master()
     double t1,t2;
     int nprocs,rank;
     int next_year;
-    int *years_bag = (int *) malloc(10 * sizeof(int));
+    int finish;
+    int years_total = YEAR_MAX - YEAR_MIN + 1;
+    int years_done = 0;
+    int *years_bag = (int *) malloc(years_total * sizeof(int));
 
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -51,9 +71,23 @@ void master()
         j++;
     }
 
+    // Envio inicial
     for(rank=1; rank < nprocs; rank++){
        next_year = rank-1;
        MPI_Send(&years_bag[next_year], 1, MPI_INT,rank,1,MPI_COMM_WORLD);
+    }
+
+    // Envia os próximos anos para processamento para os escravos liberados
+    while(years_done < years_total){
+
+        // verificar o próximo escravo liberado
+        MPI_Probe(MPI_ANY_SOURCE, DONE_TAG, MPI_COMM_WORLD, &status);
+
+        years_done++;
+        next_year++;
+
+        if(next_year < years_total)
+            MPI_Send(&years_bag[next_year], 1, MPI_INT,status.MPI_SOURCE,1,MPI_COMM_WORLD);
     }
 
     /* Envia a tag de kill para que os slaves parem o processamento */
@@ -64,13 +98,18 @@ void master()
 
 void slave()
 {
-    char buffer[134];
+    char buffer[400];
     char substring[5];
     char path[44];
     int year;
+    int finish;
+    int special;
+    int line_counter=0;
     char year_str[5];
     double temperature = -1111;
     double temp;
+    int i,l,k;
+    FILE *fp;
 
     int nprocs,rank;
     MPI_Status status;
@@ -78,7 +117,7 @@ void slave()
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // Recebe anos para processamento
+    // Recebe ano para processamento
     while(1){
 
         MPI_Recv(&year, 1, MPI_INT, 0, MPI_ANY_TAG,MPI_COMM_WORLD, &status);
@@ -87,24 +126,50 @@ void slave()
             break;
         }
 
-        printf("rank %d processando ano %d\n", rank, year);
-
+        // Apenda o ano no path
         sprintf(year_str, "%d", year);
         strcpy(path,DATASET_DIR);
         strcat(path, year_str);
 
-        FILE *fp;
+        // Computa numero de linhas no arquivo
         fp=fopen(path, "r");
-
         while (fgets(buffer, sizeof(buffer), fp)!=NULL){
-            strncpy(substring,buffer+87,5);
-            temp = atoi(substring);
-            if(temp != 9999 && temp > temperature)
-                temperature = temp;
+            line_counter++;
+        }
+        fclose(fp);
+
+        // Carrega arquivo em memória
+        char **lines = (char**)malloc(line_counter*sizeof(char));
+
+        for(k=0;k<line_counter;k++){
+                lines[k] = (char*)malloc(sizeof(buffer) * sizeof(char));
         }
 
+        l=0;
+        fp=fopen(path, "r");
+        while (fgets(buffer, sizeof(buffer), fp) !=NULL ){
+            lines[l] = chop(buffer);
+            l++;
+        }
         fclose(fp);
-        printf("%.1f\n", temperature/10);
+
+        // Calcula temperatura maxima
+#pragma omp parallel for shared(temperature) private(substring,temp,special)
+        for(i=0;i<line_counter;i++){
+             strncpy(substring,lines[i]+87,5);
+             temp = atoi(substring);
+             special = lines[i][92] - '0';
+             if(temp != 9999 && (special == 1 || special == 4 || special == 0 || special == 5 || special == 9))
+                #pragma omp critical
+                {
+                if(temp > temperature)
+                    temperature = temp;
+                }
+        }
+
+        printf("rank %d ano %d: %.1f\n", rank,year, temperature/10);
+
+        MPI_Send(0, 0, MPI_INT, 0, DONE_TAG, MPI_COMM_WORLD);
     }
 
 }
